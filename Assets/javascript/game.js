74d8cc4887c375d3d6c2f19bd495b3f63b3df380
Scott Panza <scottpanza@gmail.com>
2018-10-30 07:15:47 -0400

    new stuff

---
+++ Assets/javascript/game.js
@@ -0,0 +1,34 @@
+    var winningNumber;
+    var total;
+    var wins = 0;
+    var losses = 0;
+    var crystalPurple;
+    var crystalBlue;
+    var crystalRed;
+    var crystalYellow;
+
+
+    winningNumber = Math.floor(Math.random() * 100) + 30;
+    crystalPurple = Math.floor(Math.random() * 12) + 1;
+    crystalRed = Math.floor(Math.random() * 12) + 1;
+    crystalBlue = Math.floor(Math.random() * 12) + 1;
+    crystalYellow = Math.floor(Math.random() * 12) + 1;
+    
+    
+    
+    
+    
+    
+
+
+    
+
+        
+        
+
+    
+    
+        
+    
+    
+
